bdfac07b2ec0399d7e7be442233c0cd486147888
samolia <samolia9@gmail.com>
2021-08-31 16:00:08 +0300

    refactoring

--- library/settings.py
+++ library/settings.py
@@ -89,8 +89,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'fly_test',
-        'USER': 'samolia',
-        'PASSWORD': 'samolia',
+        'USER': DATABASE_USER,
+        'PASSWORD': DATABASE_PASSWORD,
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
