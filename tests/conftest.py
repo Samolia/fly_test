f127134945ebce2c79c5f44232a55b98d5982ac1
samolia <samolia9@gmail.com>
2021-08-31 16:05:34 +0300

    refactoring

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,15 @@
-import pytest
-from rest_framework.test import APIClient
-from model_bakery import baker
-
-
-@pytest.fixture
-def api_client():
-    return APIClient()
-
-
-@pytest.fixture
-def books_factory():
-    def factory(**kwargs):
-        return baker.make('Book', **kwargs)
-    return factory
+# import pytest
+# from rest_framework.test import APIClient
+# from model_bakery import baker
+#
+#
+# @pytest.fixture
+# def api_client():
+#     return APIClient()
+#
+#
+# @pytest.fixture
+# def books_factory():
+#     def factory(**kwargs):
+#         return baker.make('Book', **kwargs)
+#     return factory
